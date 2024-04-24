220b167f86d1f426d71fff757c17f87bbcb68de3
Hoa Nguyen (Rin) <thanhthanhhoa123@gmail.com>
2024-04-25 00:03:44 +1000

    Init project

---
+++ android/app/src/main/java/currency/exchange/MainActivity.java
@@ -0,0 +1,5 @@
+package currency.exchange;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'currency.exchange',
+  appName: 'currency-exchange',
+  webDir: 'www'
+};
+
+export default config;
